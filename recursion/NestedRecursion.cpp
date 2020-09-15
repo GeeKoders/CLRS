a208d080115017680cebfdb31c9d13d956c6605b
GeeKoders <iloveitone@gmail.com>
2020-09-15 21:39:09 +0800

    20200915 fix nested recursion

--- recursion/NestedRecursion.cpp
+++ recursion/NestedRecursion.cpp
@@ -1,18 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int fun(int n){
+int fun(int n)
+{
 
-    if(n>100)
-        return n-10 ;
-    return fun(fun(n+11)); 
-
+    if (n > 100)
+        return n - 10;
+    return fun(fun(n + 11));
 }
 
-
-int main(){
-    int r ;
-    r = fun(95) ;
-    printf("%d\n", r) ;
-    
+int main()
+{
+    int r;
+    r = fun(95);
+    printf("%d\n", r);
 }
