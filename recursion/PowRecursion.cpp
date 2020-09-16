d6e4abaeec19e5a2736aa2b5d7ae76be1e7a680a
GeeKoders <iloveitone@gmail.com>
2020-09-16 22:22:03 +0800

    20200916 add Pow recursion solution2

--- recursion/PowRecursion.cpp
+++ recursion/PowRecursion.cpp
@@ -10,8 +10,21 @@ int pow(int m, int n)
     return m * pow(m, n - 1);
 }
 
+int pow1(int m, int n){
+    if (n == 0)
+        return 1;
+
+    if(n % 2 == 0){
+        return pow1(m*m, n/2) ;
+    }else{
+        return m * pow1(m*m, (n-1)/2) ;
+    }
+
+}
+
+
 int main()
 {
-    int r = pow(2, 3) ;
+    int r = pow1(2, 4) ;
     printf("%d", r);
 }
