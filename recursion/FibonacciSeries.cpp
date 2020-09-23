ae9f7baa5c35769049695cdd3ab38875e04b501c
GeeKoders <iloveitone@gmail.com>
2020-09-23 21:58:15 +0800

    20200923 add Fibonacci series memoization

--- recursion/FibonacciSeries.cpp
+++ recursion/FibonacciSeries.cpp
@@ -18,30 +18,62 @@ using namespace std;
 */
 
 // O(2^n)
-int fibRecursive(int n){
+int fibRecursive(int n)
+{
 
-    if(n < 2) return n ;
-    return fibRecursive(n-2) + fibRecursive(n-1) ;
+    if (n < 2)
+        return n;
+    return fibRecursive(n - 2) + fibRecursive(n - 1);
 }
 // O(N)
-int fibIterative(int n){
+int fibIterative(int n)
+{
 
-    static int t0 = 0, t1 = 1, s ;
-    
-    
-    if(n < 2) return n ;
-    for(int i=2; i<=n; i++){
-        s = t0 + t1 ;
-        t0 = t1 ;
-        t1 = s ;
+    static int t0 = 0, t1 = 1, s;
+
+    if (n < 2)
+        return n;
+    for (int i = 2; i <= n; i++)
+    {
+        s = t0 + t1;
+        t0 = t1;
+        t1 = s;
     }
 
-    return s ;
-
+    return s;
 }
 
-int main(){
+int fibMemoization(int n)
+{
+    int F[10] ;
+
+    for(int i=0;i<10; i++){
+        F[i] = -1 ;
+    }
+
+    if (n < 2)
+    {
+        F[n] = n;
+        return n;
+    }
+
+    if (F[n - 2] == -1)
+    {
+        F[n - 2] = fibMemoization(n - 2);
+    }
+
+    if (F[n - 1] == -1)
+    {
+        F[n - 1] = fibMemoization(n - 1);
+    }
+
+    return F[n-2] + F[n-1] ;
+}
+
+int main()
+{
 
     // printf("%d", fibRecursive(4));
-    printf("%d", fibIterative(7));
+    // printf("%d", fibIterative(7));
+    printf("%d", fibMemoization(9)) ;
 }
