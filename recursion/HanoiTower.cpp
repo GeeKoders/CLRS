298bca711ceb6158be92e974235a65618bb90278
GeeKoders <iloveitone@gmail.com>
2020-09-24 23:12:01 +0800

    20200924 add Hanoi tower time complexity and move

--- recursion/HanoiTower.cpp
+++ recursion/HanoiTower.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 using namespace std;
 
+/*
+    A->B A->C B->C
+       (3, 1, 2, 3)
+       /    \       \
+      /      \       \ 
+(2, 1, 3, 2)  (1, 3) (2, 2, 1, 3)  
+....................
 
+n=3 15 calls = 1+2+2^2+2^3 = 2^(3+1)-1 = O(2^n)
+n=2  7 calls 
+
+move = 2^n - 1 
+*/
 void TOH(int n, int A, int B, int C){
 
     if(n>0){
